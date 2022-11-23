c4db85d7898c4fa444d021835d5ddb32b21ef998
Wei Cheng <xsnaruto@users.noreply.github.com>
2022-11-23 23:38:43 +0800

    Update mkvpropedit.sh

--- mkvpropedit.sh
+++ mkvpropedit.sh
@@ -6,4 +6,5 @@ do
     # mkvpropedit /root/tmp/"$i" --edit track:s1 --set language=chi;
     mkvpropedit /root/tmp/"$i" --edit track:s1 --set language=chi --edit track:s2 --set language=chi;
     rclone move /root/tmp/"$i" ./ -P;
+    echo "=======================================================================";
 done
