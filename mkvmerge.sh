b39147c93ff83c943d7545931b3c5979d23a1093
Wei Cheng <xsnaruto@users.noreply.github.com>
2022-11-24 02:27:25 +0800

    Update mkvmerge.sh

--- mkvmerge.sh
+++ mkvmerge.sh
@@ -3,9 +3,9 @@
 for i in *.mp4;
 do
     # echo "Merging ${i}";
-    rclone copy "$i" /root/tmp -retries 5 -P;
+    rclone copy "$i" /root/tmp --retries 5 -P;
     mkvmerge -o "/root/tmp/${i/.mp4/.mkv}" --language 1:chi /root/tmp/"$i";
-    rclone move /root/tmp/${i/.mp4/.mkv} ./ -retries 5 -P;
+    rclone move /root/tmp/${i/.mp4/.mkv} ./ --retries 5 -P;
     rm /root/tmp/$i;
     rm "$i";
 done
